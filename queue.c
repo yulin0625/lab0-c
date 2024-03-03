720a226e4e83ad774ab6575a5490945c910054f2
yulin0625 <yulin90625@gmail.com>
2024-03-04 00:34:29 +0800

    Implement q_merge function
    Implemented q_merge function to merge multiple queues into one sorted
    queue.
    
    The q_merge function merges the queues by iterating through each queue,
    splicing its elements into a temporary queue, sorting the temporary
    queue using q_sort function, and finally splicing the sorted elements
    back into the first queue.
    It returns the size of the resulting merged queue.

--- queue.c
+++ queue.c
@@ -318,5 +318,25 @@ int q_descend(struct list_head *head)
 int q_merge(struct list_head *head, bool descend)
 {
     // https://leetcode.com/problems/merge-k-sorted-lists/
-    return 0;
+    if (!head || list_empty(head)) {
+        return 0;
+    }
+
+    if (list_is_singular(head)) {
+        return list_first_entry(head, queue_contex_t, chain)->size;
+    }
+
+    LIST_HEAD(tmp);
+    queue_contex_t *it, *safe;
+    queue_contex_t *first = list_first_entry(head, queue_contex_t, chain);
+
+    list_for_each_entry_safe (it, safe, head, chain) {
+        list_splice_init(it->q, &tmp);
+    }
+
+    int size = q_size(&tmp);
+    q_sort(&tmp, descend);
+    list_splice_init(&tmp, first->q);
+
+    return size;
 }
